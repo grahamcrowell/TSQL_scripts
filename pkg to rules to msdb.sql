cbc45524945a4091ae0538e8feabd6a8df563b3e
grahamcrowell <GCrowell@vch.ca>
2016-06-08 18:21:25 -0700

    DQMF pkg to rule

---
+++ pkg to rules to msdb.sql
@@ -0,0 +1,40 @@
+USE DQMF
+GO
+
+DECLARE @pkg_name varchar(100);
+DECLARE @stg_name varchar(100);
+SET @stg_name = '%CCRS%'
+SET @pkg_name = '%CCRS%'
+
+SELECT 
+	COUNT(*) AS row_cnt
+	,pkg.PkgName
+	,stg.StageID
+	,stg.StageName
+	,br.ActionID
+	,br.ShortNameOfTest
+	,config.ConfigurationFilter
+FROM ETL_Package AS pkg
+JOIN DQMF_Schedule AS sch
+ON pkg.PkgID = sch.PkgKey
+JOIN DQMF_Stage AS stg
+ON stg.StageID = sch.StageID
+JOIN DQMF_BizRuleSchedule AS br_sch
+ON sch.DQMF_ScheduleId = br_sch.ScheduleID
+JOIN DQMF_BizRule AS br
+ON br_sch.BRID = br.BRId
+LEFT JOIN msdb.dbo.SSIS_Config AS config
+ON SUBSTRING(config.ConfigurationFilter,1,CASE WHEN CHARINDEX('.',config.ConfigurationFilter,1) = 0 THEN 100 ELSE CHARINDEX('.',config.ConfigurationFilter,1)-1 END) = pkg.PkgName
+WHERE br.IsActive = 1
+GROUP BY pkg.PkgName
+	,stg.StageID
+	,stg.StageName
+	,br.ActionID
+	,br.ShortNameOfTest
+	,config.ConfigurationFilter
+ORDER BY pkg.PkgName
+	,stg.StageID
+	,stg.StageName
+	,br.ActionID
+	,br.ShortNameOfTest
+	,config.ConfigurationFilter
