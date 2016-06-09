e3e6e983675adb588fcf262ebeaddd364a1a1821
user name <user name>
2016-06-09 02:59:56 -0700

    home

--- dqmf pkg to br.sql
+++ dqmf pkg to br.sql
@@ -1,21 +1,30 @@
 USE DQMF
 GO
 
-DECLARE @pkg_name varchar(100) = '%CCRS%';
+DECLARE @pkg_name varchar(100);
+DECLARE @stage_name varchar(100);
+DECLARE @database_name varchar(100);
+DECLARE @table_name varchar(100);
 
-SELECT COUNT(*)
-	,pkg.PkgName
+SET @pkg_name = '%CCRS%';
+SET @stage_name = NULL;
+SET @database_name = 'DSDW';
+SET @table_name = NULL;
+
+SELECT DISTINCT
+	pkg.PkgName
 	,sch.DQMF_ScheduleId
 	,stg.StageName
 	,stg.StageID
+	,br.Sequence
 	,br.ShortNameOfTest
 	,br.ActionID
 	,act.ActionName
+	,br.ActionSQL
+	,br.ConditionSQL
 	,br.SourceObjectPhysicalName
 	,br.TargetObjectPhysicalName
 	,br.FactTableObjectAttributeName
-	,OBJECT_NAME(col.object_id) AS table_name
-	,col.name AS column_name
 FROM dbo.ETL_Package AS pkg
 JOIN dbo.DQMF_Schedule AS sch
 ON pkg.PkgID = sch.PkgKey
@@ -28,41 +37,18 @@ JOIN dbo.DQMF_BizRule AS br
 ON br_sch.BRID = br.BRId
 JOIN dbo.DQMF_Action AS act
 ON br.ActionID = act.ActionID
-JOIN DSDW.sys.columns AS col
-ON br.TargetObjectAttributePhysicalName LIKE '%'+col.name+'%'
---AND (
---	br.TargetObjectPhysicalName LIKE '%'+OBJECT_NAME(col.object_id)+'%'
---	OR
---	br.TargetObjectPhysicalName LIKE '%'+OBJECT_NAME(col.object_id)+'%'
---)
+JOIN dbo.MD_Database AS db
+ON db.DatabaseId = br.DatabaseId
 WHERE 1=1
-AND pkg.PkgName LIKE @pkg_name
 AND br.IsActive = 1
-AND act.ActionName NOT IN ('Log Value')
-GROUP BY pkg.PkgName
-	,sch.DQMF_ScheduleId
-	,stg.StageName
-	,stg.StageID
-	,br.ShortNameOfTest
-	,br.ActionID
-	,act.ActionName
-	,br.SourceObjectPhysicalName
-	,br.TargetObjectPhysicalName
-	,br.FactTableObjectAttributeName
-	,OBJECT_NAME(col.object_id)
-	,col.name
-ORDER BY pkg.PkgName
-	,sch.DQMF_ScheduleId
-	,stg.StageName
-	,stg.StageID
-	,br.ShortNameOfTest
-	,br.ActionID
-	,act.ActionName
-	,br.SourceObjectPhysicalName
-	,br.TargetObjectPhysicalName
-	,br.FactTableObjectAttributeName
-	,OBJECT_NAME(col.object_id)
-	,col.name
+AND (@pkg_name IS NULL OR pkg.PkgName LIKE @pkg_name)
+AND (@stage_name IS NULL OR stg.StageName LIKE @stage_name)
+AND (@database_name IS NULL OR db.DatabaseName LIKE @database_name)
+AND (@table_name IS NULL 
+		OR br.SourceObjectPhysicalName LIKE @table_name
+		OR br.TargetObjectPhysicalName LIKE @table_name
+)
+ORDER BY pkg.PkgName, stg.StageName, br.Sequence ASC
 
 ----ActionID = 0 -> lookup
 ---- = 1 -> identify bad records:
---
+++ uspHelp/DQMF_ShortCuts.sql
@@ -0,0 +1,10 @@
+USE DQMF
+GO
+
+DECLARE @stage_name varchar(100);
+SET @stage_name;
+
+
+DECLARE @pkg_name varchar(100);
+SET @pkg_name = '%CCRS%';
+
