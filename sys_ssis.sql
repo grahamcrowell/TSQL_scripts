bd7fe499f338134d4c198f608e445d3039a72d31
grahamcrowell <GCrowell@vch.ca>
2016-08-22 20:20:11 -0700

    tables size

---
+++ sys_ssis.sql
@@ -0,0 +1,51 @@
+-- Integration Services Tables (Transact-SQL) https://msdn.microsoft.com/en-us/library/ms181701.aspx
+-- SQL Server Agent Tables (Transact-SQL) https://msdn.microsoft.com/en-us/library/ms181367.aspx
+
+:connect STDBDECSUP02
+USE DQMF
+GO
+
+;WITH msdb_package_path AS (
+	SELECT dir.foldername, dir.folderid, dir.parentfolderid,0 AS Level, CAST('' AS varchar(200)) AS dirpath
+	FROM msdb.dbo.sysssispackagefolders AS dir
+	WHERE dir.parentfolderid IS NULL
+	UNION ALL
+	SELECT dir.foldername, dir.folderid, dir.parentfolderid, Level + 1, CAST(dirpath AS varchar(100)) + '\' + CAST(dir.foldername AS varchar(99)) AS dirpath
+	FROM msdb.dbo.sysssispackagefolders AS dir
+	JOIN msdb_package_path
+	ON dir.parentfolderid = msdb_package_path.folderid
+), msdb_package AS (
+	SELECT CAST(dirpath AS varchar(100)) + '\' + CAST(pkg.name AS varchar(99)) AS package_path, msdb_package_path.*, pkg.name AS package_name, pkg.id
+	FROM msdb_package_path
+	JOIN msdb.dbo.sysssispackages AS pkg
+	ON pkg.folderid = msdb_package_path.folderid
+), msdb_job_step AS (
+	SELECT 
+		job.name AS job_name, job.date_created, job.date_modified
+		,step.step_name, step.step_id, step.subsystem, step.command
+		,msdb_package.package_name, msdb_package.package_path
+		,step.last_run_date, step.last_run_duration
+		,CASE 
+			WHEN act.run_requested_source = 1 THEN 'SOURCE_SCHEDULER'
+			WHEN act.run_requested_source = 4 THEN 'SOURCE_USER'
+			WHEN act.run_requested_source IS NULL THEN NULL
+			ELSE 'see documentation'
+		END AS execution_source
+			
+	FROM msdb.dbo.sysjobsteps AS step
+	JOIN msdb.dbo.sysjobs AS job
+	ON step.job_id = job.job_id
+	JOIN msdb.dbo.sysjobactivity AS act
+	ON job.job_id = act.job_id
+	LEFT JOIN msdb_package
+	ON step.command LIKE '%' + CAST(msdb_package.package_path AS varchar(500)) + '%' 
+)
+SELECT msdb_job_step.*
+FROM msdb_job_step
+WHERE 1=1
+AND msdb_job_step.job_name NOT IN 
+(
+	SELECT PkgName 
+	FROM DQMF.dbo.ETL_Package
+)
+ORDER BY last_run_date DESC
---
+++ table_size.sql
@@ -0,0 +1,47 @@
+USE DSDW
+GO
+
+SELECT 
+    t.NAME AS TableName,
+    s.Name AS SchemaName,
+    p.rows AS RowCounts,
+    SUM(a.total_pages) * 8 AS TotalSpaceKB, 
+    SUM(a.used_pages) * 8 AS UsedSpaceKB, 
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedSpaceMB
+FROM 
+    sys.tables t
+INNER JOIN      
+    sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN 
+    sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN 
+    sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN 
+    sys.schemas s ON t.schema_id = s.schema_id
+WHERE 
+    t.NAME NOT LIKE 'dt%' 
+    AND t.is_ms_shipped = 0
+    AND i.OBJECT_ID > 255 
+GROUP BY 
+    t.Name, s.Name, p.Rows
+ORDER BY 
+    SUM(a.total_pages) DESC
+
+
+
+USE DSDW 
+GO
+
+EXEC sp_MSforeachtable @command1 = '
+DECLARE @rcount int;
+
+IF OBJECT_SCHEMA_NAME(OBJECT_ID(''?'')) = ''SnapShot'' OR 1=1
+BEGIN
+	SELECT @rcount=COUNT(*) FROM ?;
+	INSERT INTO ##tab VALUES (''?'', @rcount);
+END
+', @precommand = 'CREATE TABLE ##tab (
+	TableName varchar(200)
+	,rcount int
+);',@postcommand = 'SELECT * FROM ##tab order by rcount desc; DROP TABLE ##tab;'
+
