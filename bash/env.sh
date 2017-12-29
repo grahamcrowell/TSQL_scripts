0305126148c6c1be132939364c221308684a42ca
gcrowell <graham.crowell@gmail.com>
2017-12-29 11:37:03 -0800

    awk and dirdiff

--- bash/env.sh
+++ bash/env.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/sh
 
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-CYAN=$(tput setaf 6)
-GREY=$(tput setaf 240)
-RESET_COLOR=$(tput sgr0)
+export RED=$(tput setaf 1)
+export GREEN=$(tput setaf 2)
+export YELLOW=$(tput setaf 3)
+export CYAN=$(tput setaf 6)
+export GREY=$(tput setaf 240)
+export RESET_COLOR=$(tput sgr0)
 
-RELEASE_BRANCH=RELEASE-20170721-VEYRON
-HGDIR=$HOME/Documents/hg/
+export RELEASE_BRANCH=RELEASE-20171110-VEYRON
+export HGDIR=$HOME/Documents/hg
+export DIFFDIR=$HGDIR/code_review_diffs
+
+export PATH=$HOME/Documents/git/scripts/bash:/usr/local/bin:$PATH
+export PATH=$HOME/Documents/git/scripts/python:$PATH
--- bash/jupyter_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# 1. install brew package manager
-# 2. install python3
-# 3. install 
