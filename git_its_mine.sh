20fa368bb7b0cfd7e410d6c30a9407c55b235be9
grahamcrowell <graham.crowell@gmail.com>
2019-08-04 19:51:28 -0400

    improve git_its_mine

--- git_its_mine.sh
+++ git_its_mine.sh
@@ -4,8 +4,8 @@ REPO_DIR=$PWD
 printf "${YELLOW}updating git history of repo: ${REPO_DIR}\n${RESET}";
 CORRECT_NAME=`git config --get user.name`
 CORRECT_EMAIL=`git config --get user.email`
-printf "COMMITTER and AUTHOR name: ${CORRECT_NAME}\n"
-printf "COMMITTER and AUTHOR email: ${CORRECT_EMAIL}\n"
+printf "COMMITTER and AUTHOR name: ${GREEN}${CORRECT_NAME}${RESET}\n"
+printf "COMMITTER and AUTHOR email: ${GREEN}${CORRECT_EMAIL}${RESET}\n"
 
 printf "\n${YELLOW}press ENTER to continue${RESET}\n"
 read
@@ -24,4 +24,4 @@ then
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
 fi
-'#cd .. --tag-name-filter cat -- --branches --tags
+' --tag-name-filter cat -- --branches --tags
